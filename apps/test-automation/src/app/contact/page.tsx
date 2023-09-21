04e87792f4c2851e5db9106e2d33ff410502f567
Arjan Blok <arjan.blok@gmail.com>
2023-09-21 20:19:41 +0200

    Update button text

--- apps/test-automation/src/app/contact/page.tsx
+++ apps/test-automation/src/app/contact/page.tsx
@@ -4,7 +4,7 @@ export default function Page(): JSX.Element {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <Heading title="Contact Us" />
-      <Button text="Click me"/>
+      <Button text="Click me!"/>
     </main>
   );
 }
